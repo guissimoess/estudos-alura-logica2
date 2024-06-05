62c009fdf232ba9ad5f9ce6f02f0e941ec78a9d8
Guilherme Simões <guilhermessimoescontato@gmail.com>
2024-06-05 16:51:04 -0300

    finalizando exercicios e sobre listas

---
+++ exericio3.js
@@ -0,0 +1,9 @@
+let listaGenerica = [];
+
+let linguagensDeProgramacao = ['Javascript', 'C', 'C++', 'Kotlin', 'Python'];
+linguagensDeProgramacao.push('Java', 'Ruby', 'Golang');
+
+let listaNomes = ['Pedro', 'Joao', 'Nathalia'];
+console.log(listaNomes[0]);
+console.log(listaNomes[1]);
+console.log(listaNomes[listaNomes.length - 1]);
